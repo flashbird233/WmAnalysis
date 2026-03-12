521a47bd04eb2ffb68139c9076d06be02792428c
flashbird233 <moqianchen9811@gmail.com>
2026-03-12 17:38:16 +0800

    设置页面标题

--- main_page.py
+++ main_page.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.write("""
-# My first app
-""")
+def main():
+    # 为页面添加标题: 维明对公客户指标分析 居中
+    st.markdown('<h1 style="text-align: center;">维明对公客户指标分析</h1>', unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
