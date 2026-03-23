fb80a3a34da5e910816e8007b6e1fa6890d5707a
flashbird233 <moqianchen9811@gmail.com>
2026-03-23 11:33:08 +0800

    新增WmAnalysis文件,作为后续网站运行主文件, 并在主页增加了上传之前(前一天)数据的选项

--- home_page.py
+++ home_page.py
@@ -26,13 +26,16 @@ def home_page():
     st.markdown('<h2 style="text-align: center;">请上传数据文件并进行相关参数设置</h2>', unsafe_allow_html=True)
 
     # 设置上传数据文件的输入框
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col2:
         # 让用户上传去年底数据, 该数据文件需要从第四行开始读取
-        st.session_state.last_year_data = st.file_uploader('上传去年底数据文件: ', type=['xlsx', 'xls'])
+        st.session_state.last_year_data = st.file_uploader('请上传去年底数据文件: ', type=['xlsx', 'xls'])
     with col3:
-        # 让客户上传当前数据文件, 该数据文件需要从第四行开始读取
-        st.session_state.this_year_data = st.file_uploader('上传当前数据文件: ', type=['xlsx', 'xls'])
+        # 请用户上传之前数据文件, 该数据文件需要从第四行开始读取
+        st.session_state.previous_data = st.file_uploader('请上传之前数据文件: ', type=['xlsx', 'xls'])
+    with col4:
+        # 让用户上传当前数据文件, 该数据文件需要从第四行开始读取
+        st.session_state.current_data = st.file_uploader('请上传当前数据文件: ', type=['xlsx', 'xls'])
 
 
 if __name__ == '__main__':
